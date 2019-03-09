aac295f3680b546bbf1c202e215d41e570b108e2
ubuntu <1009667605@qq.com>
2019-03-09 12:27:34 +0800

    :o: date:2019-03-09 12:27:34 Commit file

--- shell/bash192.sh
+++ shell/bash192.sh
@@ -3,3 +3,15 @@
 # 192. 统计词频
 
 set -ue
+
+# 获取内容
+content=`cat words.txt`
+
+echo $content
+
+#这里是将var中的,替换为空格
+echo ${content//'  '/ }
+
+tt=${content//'  '/ }
+
+echo $tt|awk -F "[: ]+" '{print $1}'
