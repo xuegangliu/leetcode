b80fe3bcc4d04f30e5b0b1824facffd9970567a6
ubuntu <1009667605@qq.com>
2019-03-26 17:49:45 +0800

    :link: date:2019-03-26 17:49:45 Commit file

---
+++ docs/linux/basic_shell.md
@@ -0,0 +1,12 @@
+基础shell
+---
+
+
+#### tr
+- [introd](https://blog.csdn.net/jeffreyst_zb/article/details/8047065 ':target=_blank')
+
+#### sort
+
+#### uniq
+
+
--- shell/192/bash192.sh
+++ shell/192/bash192.sh
@@ -7,14 +7,18 @@
 
 set -ue
 
-# 获取内容
-content=`cat file192.txt`
+# 方式1
+# tr\sort\uniq\awk命令使用
+cat file192.txt|tr ' ' '\n'|tr -s '\n'|sort -|uniq -c|sort -rn|awk '{print$2" "$1}'
 
-echo $content
+
+# 获取内容
+#content=`cat file192.txt`
+#echo $content
 
 #这里是将var中的,替换为空格
-echo ${content//'  '/ }
+#echo ${content//'  '/ }
 
-tt=${content//'  '/ }
+#tt=${content//'  '/ }
 
-echo $tt|awk -F "[: ]+" '{print $1}'
+#echo $tt|awk -F "[: ]+" '{print $1}'
