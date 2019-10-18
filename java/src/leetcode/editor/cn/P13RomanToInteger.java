4a2134be4ec2197e97b4c68770fcbf556ea8298d
hrt-lxg <liuxuegang@hrtpayment.com>
2019-10-18 16:45:52 +0800

    Add Solution number to 13 by new solution

--- java/src/leetcode/editor/cn/P13RomanToInteger.java
+++ java/src/leetcode/editor/cn/P13RomanToInteger.java
@@ -83,6 +83,31 @@ class Solution {
         map.put("M",1000);
         int len=s.length();
         int sum=0;
+        for (int i = 0; i < len-1; i++) {
+            String a=String.valueOf(s.charAt(i));
+            String b=String.valueOf(s.charAt(i+1));
+            if(map.get(a).compareTo(map.get(b))<0){
+                sum=sum-map.get(a);
+            }else{
+                sum=sum+map.get(a);
+            }
+        }
+        sum=sum+map.get(String.valueOf(s.charAt(len-1)));
+        return sum;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+    public int romanToIntA(String s) {
+        Map<String,Integer> map=new HashMap<String,Integer>();
+        map.put("I",1);
+        map.put("V",5);
+        map.put("X",10);
+        map.put("L",50);
+        map.put("C",100);
+        map.put("D",500);
+        map.put("M",1000);
+        int len=s.length();
+        int sum=0;
         List<String> list = new ArrayList<String>();
         for (int i = 0; i < len; i++) {
             String a=String.valueOf(s.charAt(i));
@@ -109,7 +134,4 @@ class Solution {
         }
         return sum;
     }
-}
-//leetcode submit region end(Prohibit modification and deletion)
-
 }
