379b835dff3052fa7ebfb45d73af85a0f1bbfaf2
hrt-lxg <liuxuegang@hrtpayment.com>
2019-10-29 11:36:19 +0800

    Add Solution number to 35

---
+++ java/src/leetcode/editor/cn/P35SearchInsertPosition.java
@@ -0,0 +1,63 @@
+//给定一个排序数组和一个目标值，在数组中找到目标值，并返回其索引。如果目标值不存在于数组中，返回它将会被按顺序插入的位置。 
+//
+// 你可以假设数组中无重复元素。 
+//
+// 示例 1: 
+//
+// 输入: [1,3,5,6], 5
+//输出: 2
+// 
+//
+// 示例 2: 
+//
+// 输入: [1,3,5,6], 2
+//输出: 1
+// 
+//
+// 示例 3: 
+//
+// 输入: [1,3,5,6], 7
+//输出: 4
+// 
+//
+// 示例 4: 
+//
+// 输入: [1,3,5,6], 0
+//输出: 0
+// 
+// Related Topics 数组 二分查找
+
+package leetcode.editor.cn;
+//Java：搜索插入位置
+public class P35SearchInsertPosition{
+    public static void main(String[] args) {
+        Solution solution = new P35SearchInsertPosition().new Solution();
+        // TO TEST
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},5));
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},7));
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},0));
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},2));
+        System.out.println(solution.searchInsert(new int[]{1,3,5,6},4));
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int len=nums.length;
+        for (int i = 0; i < len; i++) {
+            // 找到存在的位置
+            if(nums[i]>target){
+                return i;
+            }else{
+                if(nums[i]==target){
+                    return i;
+                }
+            }
+        }
+        return len;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
