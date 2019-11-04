06efd0276bb2d2dd6ef0d086d9c4a7093ccc139f
hrt-lxg <liuxuegang@hrtpayment.com>
2019-11-04 15:25:21 +0800

    Add Solution number to 70,155,703

---
+++ java/src/leetcode/editor/cn/P155MinStack.java
@@ -0,0 +1,92 @@
+//设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。 
+//
+// 
+// push(x) -- 将元素 x 推入栈中。 
+// pop() -- 删除栈顶的元素。 
+// top() -- 获取栈顶元素。 
+// getMin() -- 检索栈中的最小元素。 
+// 
+//
+// 示例: 
+//
+// MinStack minStack = new MinStack();
+//minStack.push(-2);
+//minStack.push(0);
+//minStack.push(-3);
+//minStack.getMin();   --> 返回 -3.
+//minStack.pop();
+//minStack.top();      --> 返回 0.
+//minStack.getMin();   --> 返回 -2.
+// 
+// Related Topics 栈 设计
+
+package leetcode.editor.cn;
+
+import java.util.Stack;
+
+//Java：最小栈
+public class P155MinStack{
+    public static void main(String[] args) {
+        MinStack minStack = new P155MinStack().new MinStack();
+        // TO TEST
+        minStack.push(-2);
+        minStack.push(0);
+        minStack.push(-3);
+        System.out.println(minStack.getMin());
+        minStack.pop();
+        System.out.println(minStack.top());
+        System.out.println(minStack.getMin());
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class MinStack {
+    private int value[];
+    private int size;
+    private int cur;
+
+    /** initialize your data structure here. */
+    public MinStack() {
+        this.size=0;
+        this.cur=0;
+        this.value=new int[65536];
+    }
+
+    public void push(int x) {
+        this.value[cur]=x;
+        this.cur++;
+        this.size++;
+    }
+
+    public void pop() {
+        if(this.size>0) {
+            this.size--;
+            this.cur--;
+        }
+    }
+
+    public int top() {
+        return this.value[cur-1];
+    }
+
+    public int getMin() {
+        int t=value[0];
+        for (int i = 0; i < size; i++) {
+            if(t>value[i]){
+                t=value[i];
+            }
+        }
+        return t;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+}
---
+++ java/src/leetcode/editor/cn/P703KthLargestElementInAStream.java
@@ -0,0 +1,79 @@
+//设计一个找到数据流中第K大元素的类（class）。注意是排序后的第K大元素，不是第K个不同的元素。 
+//
+// 你的 KthLargest 类需要一个同时接收整数 k 和整数数组nums 的构造器，它包含数据流中的初始元素。每次调用 KthLargest.add，返回当前数据流中第K大的元素。 
+//
+// 示例: 
+//
+// 
+//int k = 3;
+//int[] arr = [4,5,8,2];
+//KthLargest kthLargest = new KthLargest(3, arr);
+//kthLargest.add(3);   // returns 4
+//kthLargest.add(5);   // returns 5
+//kthLargest.add(10);  // returns 5
+//kthLargest.add(9);   // returns 8
+//kthLargest.add(4);   // returns 8
+// 
+//
+// 说明: 
+//你可以假设 nums 的长度≥ k-1 且k ≥ 1。 
+// Related Topics 堆
+
+package leetcode.editor.cn;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+//Java：数据流中的第K大元素
+public class P703KthLargestElementInAStream{
+    public static void main(String[] args) {
+        int k = 3;
+        int[] arr = new int[]{4,5,8,2};
+        KthLargest solution = new P703KthLargestElementInAStream().new KthLargest(k,arr);
+        // TO TEST
+        System.out.println(solution.add(3));
+        System.out.println(solution.add(5));
+        System.out.println(solution.add(10));
+        System.out.println(solution.add(9));
+        System.out.println(solution.add(4));
+    }
+    
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class KthLargest {
+    final PriorityQueue<Integer> pq;
+    final int k;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        pq = new PriorityQueue<>(k);
+
+        for(int i : nums){//对传进来的int数组遍历
+            add(i);
+        }
+    }
+
+    public int add(int val) {
+        if(pq.size() < k)//如果队列中的数量少于K，直接添加入优先队列，优先队列会自动维持小顶堆
+            pq.offer(val);
+        else{
+            if(pq.peek() < val){//否则队列中的数量大于或者等于K，优先队列中的最小数字小于新的数据，优先队列中的顶堆要被移除，并且添加入新的数据进优先队列
+                pq.poll();
+                pq.offer(val);
+
+            }
+
+        }
+        return pq.peek();//返回当前第K大的数
+
+    }
+}
+
+/**
+ * Your KthLargest object will be instantiated and called as such:
+ * KthLargest obj = new KthLargest(k, nums);
+ * int param_1 = obj.add(val);
+ */
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
